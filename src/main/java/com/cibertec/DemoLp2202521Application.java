779123005fc33af8d4708c86b8ef84d3d5c5b186
unknown <SUITE@SUITEA103-SERV.cibertec.edu.pe>
2025-06-25 10:26:37 -0500

    1er Commit

---
+++ src/main/java/com/cibertec/DemoLp2202521Application.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLp2202521Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLp2202521Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/DemoLp2202521ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLp2202521ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
